3ce78b4ded52cf55b9ea648e59c2d959741dafb6
cameronwoo <102308812+cameronwoo@users.noreply.github.com>
2024-04-24 11:41:54 +0800

    Update __init__.py

--- atm-lbf/src/runners/__init__.py
+++ atm-lbf/src/runners/__init__.py
@@ -1,9 +1,7 @@
 REGISTRY = {}
 
 from .episode_runner import EpisodeRunner
-print('REGISTRY 断点1')
 REGISTRY["episode"] = EpisodeRunner
-print('REGISTRY 断点2')
+
 from .parallel_runner import ParallelRunner
 REGISTRY["parallel"] = ParallelRunner
-print('REGISTRY 断点3')
